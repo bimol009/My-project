7da5d6104815dd8f6198bda76a14ceaa453f4341
bimol009 <110928529+bimol009@users.noreply.github.com>
2023-07-01 23:45:27 +0600

    new

--- src/pages/Skill/Skill.jsx
+++ src/pages/Skill/Skill.jsx
@@ -5,12 +5,15 @@ import ScrollAnimation from "react-animate-on-scroll";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
 import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import "swiper/css/effect-cards";
 
-import { EffectCoverflow, Pagination, Navigation } from "swiper";
+
+
+// import required modules
+import { EffectCards } from "swiper";
+
 import { Link } from "react-scroll/modules";
 
 const Skill = () => {
@@ -21,27 +24,12 @@ const Skill = () => {
       </div>
       <ScrollAnimation animateIn="animate__pulse">
         <div className="container">
-          <Swiper
-            effect={"coverflow"}
-            grabCursor={true}
-            centeredSlides={true}
-            loop={true}
-            slidesPerView={"auto"}
-            coverflowEffect={{
-              rotate: 0,
-              stretch: 0,
-              depth: 100,
-              modifier: 2.5,
-            }}
-            pagination={{ el: ".swiper-pagination", clickable: true }}
-            navigation={{
-              nextEl: ".swiper-button-next",
-              prevEl: ".swiper-button-prev",
-              clickable: true,
-            }}
-            modules={[EffectCoverflow, Pagination, Navigation]}
-            className="swiper_container"
-          >
+        <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+      >
             <SwiperSlide>
               <img
                 src="https://i.ibb.co/q59PggP/2400-1260-rw-blog-node-js.png"
@@ -91,15 +79,7 @@ const Skill = () => {
               />
             </SwiperSlide>
 
-            <div className="slider-controler">
-              <div className="swiper-button-prev slider-arrow">
-                <ion-icon name="arrow-back-outline"></ion-icon>
-              </div>
-              <div className="swiper-button-next slider-arrow">
-                <ion-icon name="arrow-forward-outline"></ion-icon>
-              </div>
-              <div className="swiper-pagination"></div>
-            </div>
+           
           </Swiper>
         </div>
       </ScrollAnimation>
