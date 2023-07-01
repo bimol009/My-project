5ad583936c33bdc806641c82ea61beb8087996ae
bimol009 <110928529+bimol009@users.noreply.github.com>
2023-07-01 20:48:14 +0600

    final

--- src/Particles/ParticlesBackground.jsx
+++ src/Particles/ParticlesBackground.jsx
@@ -1,98 +1,87 @@
 import { useCallback } from "react";
-
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
 
-
 const ParticlesBackground = () => {
-    const particlesInit = useCallback(async engine => {
-        console.log(engine);
-      
-        await loadFull(engine);
-    }, []);
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadFull(engine);
+  }, []);
 
-    const particlesLoaded = useCallback(async container => {
-        await console.log(container);
-    }, []);
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
 
   return (
-    
     <Particles
-  id="tsparticles"
-  init={particlesInit}
-  loaded={particlesLoaded}
-  options={{
- 
-    fpsLimit: 160,
-    interactivity: {
-      events: {
-        onClick: {
-          enable: true,
-          mode: "push",
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fpsLimit: 160,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            repulse: {
+              distance: 100,
+              duration: 0.4,
+            },
+          },
         },
-        onHover: {
-          enable: true,
-          mode: "repulse",
+        particles: {
+          color: {
+            value: ["#ff0000", "#00ff00", "#0000ff"],
+          },
+          links: {
+            color: "#00FFFF", // Change the link color to "#00FFFF"
+            distance: 150,
+            enable: true,
+            opacity: 0.3,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 4,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 600,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.3,
+          },
+          shape: {
+            type: ["circle", "square", "triangle"],
+          },
+          size: {
+            value: { min: 1, max: 3 },
+          },
         },
-        resize: true,
-      },
-      modes: {
-        push: {
-          quantity: 2,
-        },
-        repulse: {
-          distance: 100,
-          duration: 0.4,
-        },
-      },
-    },
-    particles: {
-      color: {
-        value: ["#ff0000", "#00ff00", "#0000ff"],
-      },
-      links: {
-        color: "#FFFF00",
-        distance: 150,
-        enable: true,
-        opacity: 0.3,
-        width: 1,
-      },
-      collisions: {
-        enable: true,
-      },
-      move: {
-        direction: "none",
-        enable: true,
-        outModes: {
-          default: "bounce",
-        },
-        random: false,
-        speed: 4,
-        straight: false,
-      },
-      number: {
-        density: {
-          enable: true,
-          area: 600,
-        },
-        value: 80,
-      },
-      opacity: {
-        value: 0.3,
-      },
-      shape: {
-        type: ["circle", "square", "triangle"],
-      },
-      size: {
-        value: { min: 1, max: 3 },
-      },
-    },
-    detectRetina: true,
-  }}
-/>
-
-   
-
+        detectRetina: true,
+      }}
+    />
   );
 };
 
